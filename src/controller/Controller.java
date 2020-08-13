337292405de797e6bac2bb4430e7bcbc586d115d
David <dmartinezgonzalez524@gmail.com>
2020-08-13 02:32:07 -0400

    creation of class controller
    creation of class controller, just I moved class from package 'sample' to 'controller'. Indeed sample.fxml is modified

---
+++ src/controller/Controller.java
@@ -0,0 +1,20 @@
+package controller;
+
+import source.Structure;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+    private ArrayList<Structure> structures;
+
+    public Controller() {
+        super();
+        this.structures = new ArrayList<>();
+    }
+
+    public Controller(ArrayList<Structure> structures) {
+        super();
+        this.structures = structures;
+    }
+}
